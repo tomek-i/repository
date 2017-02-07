dda65397072099e40937e8bd8e3a950eb37b0288
Thomas Iwainski <tiwainski@warburnestate.com.au>
2017-02-07 13:20:54 +1100

    changed naming convention of project and assembly information added nuget packages

--- Repository.UnitTests/Properties/AssemblyInfo.cs
+++ Repository.UnitTests/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Repository.UnitTests")]
+[assembly: AssemblyTitle("Thomas Iwainski Repository.UnitTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Repository.UnitTests")]
-[assembly: AssemblyCopyright("Copyright © Thomas Iwainski 2016")]
+[assembly: AssemblyCompany("Thomas Iwainski")]
+[assembly: AssemblyProduct("Thomas Iwainski Repository.UnitTests")]
+[assembly: AssemblyCopyright("Copyright © Thomas Iwainski 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
