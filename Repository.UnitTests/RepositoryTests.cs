0f957355a2340f17f71d9eb86a2e21c29604af40
Thomas Iwainski <tiwainski@warburnestate.com.au>
2017-02-28 13:46:29 +1100

    added repository test and missing packages

--- Repository.UnitTests/RepositoryTests.cs
+++ Repository.UnitTests/RepositoryTests.cs
@@ -1,19 +1,38 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Moq;
+using NUnit.Framework;
 
 namespace TI.Repository.UnitTests
 {
     [TestFixture]
+    public class RepositoryInterfaceTests
+    {
+        
+    }
+    [TestFixture]
+    public class RepositoryStorageTests
+    {
+
+    }
+
+    [TestFixture]
     public class RepositoryTests
     {
 
+        
         [Test]
-        public void UnitAtTest_Conditions_ExpectedBehaviour()
+        public void IsDisposed_ObjectIsNotDisposed_ReturnFalse()
         {
             //ARRANGE
+            Mock<Repository<int>> moq = new Mock<Repository<int>>();
 
             //ACT
+            var result = moq.Object.IsDisposed;
+
             //ASSERT
-            Assert.Fail();
+            Assert.IsFalse(result);
         }
     }
 }
